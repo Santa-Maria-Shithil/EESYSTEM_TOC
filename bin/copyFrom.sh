ff92b5ecd06efe26653457eef1e0cae0b36904e0
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-13 18:54:14 -0500

    update

--- bin/copyFrom.sh
+++ bin/copyFrom.sh
@@ -5,6 +5,6 @@ do
    sudo docker cp client1:/copilot/client-${i}.latency.all.txt /home/users/sshithil/copilot/logs/client-${i}.latency.all.txt
    sudo docker cp client1:/copilot/client-${i}.latency.orig.txt /home/users/sshithil/copilot/logs/client-${i}.latency.orig.txt
 sudo docker cp client1:/copilot/client-${i}.latency.percentiles.txt /home/users/sshithil/copilot/logs/client-${i}.latency.percentiles.txt
-sudo docker cp client1:/copilot/client-${i}.timestaps.orig.txt /home/users/sshithil/copilot/logs/client-${i}.timestamps.orig.txt
+sudo docker cp client1:/copilot/client-${i}.timestamps.orig.txt /home/users/sshithil/copilot/logs/client-${i}.timestamps.orig.txt
 sudo docker cp client1:/copilot/client-${i}.tputlat.txt /home/users/sshithil/copilot/logs/client-${i}.tputlat.txt
 done
