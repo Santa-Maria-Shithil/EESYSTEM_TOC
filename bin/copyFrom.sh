b704534fc2fcb77f9b18e91b09185885cb1827ff
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-13 16:50:49 -0500

    update

--- bin/copyFrom.sh
+++ bin/copyFrom.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 for i in {1..90}
 do
-   sudo docker cp client5:/epaxos/logs/c_${i}.txt /home/users/sshithil/epaxos/logs/c_${i}.txt
+   sudo docker cp client1:/copilot/client-${i}.throughput.txt /home/users/sshithil/epaxos/logs/client-${i}.throughput.txt
+   sudo docker cp client1:/copilot/client-${i}.latency.all.txt /home/users/sshithil/epaxos/logs/client-${i}.latency.all.txt
+   sudo docker cp client1:/copilot/client-${i}.latency.orig.txt /home/users/sshithil/epaxos/logs/client-${i}.latency.orig.txt
+sudo docker cp client1:/copilot/client-${i}.latency.percentiles.txt /home/users/sshithil/epaxos/logs/client-${i}.latency.percentiles.txt
+sudo docker cp client1:/copilot/client-${i}.timestaps.orig.txt /home/users/sshithil/epaxos/logs/client-${i}.timestaps.orig.txt
+sudo docker cp client1:/copilot/client-${i}.tputlat.txt /home/users/sshithil/epaxos/logs/client-${i}.tputlat.txt
 done
