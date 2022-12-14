efc171a9e6ccb1d8f2857e5fcbba373a415aa838
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-14 18:52:58 -0500

    update

--- bin/initClient.sh
+++ bin/initClient.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #bash bin/initClient.sh
 MIP="172.76.0.2"
-MASTERNAME="master"
+MASTERNAME="master1"
 MASTERPORT=5087
 CLIENTNAME="client1"  #modify client name
 CLIENTIP="172.76.0.20" #ip start with IP address 172.75.0.200
@@ -12,7 +12,7 @@ docker stop ${CLIENTNAME}
 docker rm ${CLIENTNAME}
 docker rmi ${CLIENTNAME}
 docker build --tag ${CLIENTNAME} .
-docker run --cpuset-cpus="13-63" --memory="8g" --net Paxos_Network1 --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e TYPE="client" --name ${CLIENTNAME} ${CLIENTNAME}
+docker run --cpuset-cpus="43-63" --memory="8g" --net Paxos_Network1 --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e TYPE="client" --name ${CLIENTNAME} ${CLIENTNAME}
 
 
 
