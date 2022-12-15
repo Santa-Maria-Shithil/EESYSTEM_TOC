1402b5000bd4c8829ee20b0bef7139194c83b18c
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-14 22:52:43 -0500

    update

--- bin/copyFrom.sh
+++ bin/copyFrom.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 for i in {1..90}
 do
-   sudo docker cp client5:/copilot/client-${i}.throughput.txt /home/users/sshithil/copilot/logs/client-${i}.throughput.txt
-   sudo docker cp client5:/copilot/client-${i}.latency.all.txt /home/users/sshithil/copilot/logs/client-${i}.latency.all.txt
-   sudo docker cp client5:/copilot/client-${i}.latency.orig.txt /home/users/sshithil/copilot/logs/client-${i}.latency.orig.txt
-sudo docker cp client5:/copilot/client-${i}.latency.percentiles.txt /home/users/sshithil/copilot/logs/client-${i}.latency.percentiles.txt
-sudo docker cp client5:/copilot/client-${i}.timestamps.orig.txt /home/users/sshithil/copilot/logs/client-${i}.timestamps.orig.txt
-sudo docker cp client5:/copilot/client-${i}.tputlat.txt /home/users/sshithil/copilot/logs/client-${i}.tputlat.txt
+   sudo docker cp client1:/copilot/client-${i}.throughput.txt /home/users/sshithil/copilot/logs/client-${i}.throughput.txt
+   sudo docker cp client1:/copilot/client-${i}.latency.all.txt /home/users/sshithil/copilot/logs/client-${i}.latency.all.txt
+   sudo docker cp client1:/copilot/client-${i}.latency.orig.txt /home/users/sshithil/copilot/logs/client-${i}.latency.orig.txt
+sudo docker cp client1:/copilot/client-${i}.latency.percentiles.txt /home/users/sshithil/copilot/logs/client-${i}.latency.percentiles.txt
+sudo docker cp client1:/copilot/client-${i}.timestamps.orig.txt /home/users/sshithil/copilot/logs/client-${i}.timestamps.orig.txt
+sudo docker cp client1:/copilot/client-${i}.tputlat.txt /home/users/sshithil/copilot/logs/client-${i}.tputlat.txt
 done
