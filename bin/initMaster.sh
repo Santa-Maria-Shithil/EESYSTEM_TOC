407ee0c9e1e72728a8a36b47e31910f3baf6bc67
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-14 22:23:53 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -7,5 +7,5 @@ docker rm ${MASTERNAME}
 docker rmi ${MASTERNAME}
 docker network rm Paxos_Network4
 docker build --tag ${MASTERNAME} .
-docker network create --subnet=172.79.0.0/16 Paxos_Network3
+docker network create --subnet=172.79.0.0/16 Paxos_Network4
 docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network4 --ip=${MIP} -p 8087:8087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=8087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME} 
