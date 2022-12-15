5d28cb65ed67d21a6afadff3b7dee19147bee395
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-14 19:34:51 -0500

    update

--- bin/client2.sh
+++ bin/client2.sh
@@ -5,7 +5,7 @@ MASTERNAME="master3"
 MASTERPORT=6087
 CLIENTNAME="client2"  #modify client name
 CLIENTIP="172.77.0.20" #ip start with IP address 172.75.0.200
-CLIENTPORT=8090 #port start with 7270
+CLIENTPORT=6098 #port start with 7270
 NCLIENTS=20 # minimum 1
 NREQ=50000 # total number of req per client
 docker stop ${CLIENTNAME}
