b17edaa38bb0d3a8390ef1980c804ed2d61b5f3f
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2022-12-19 16:21:22 -0500

    update

--- bin/cpuUsage2.sh
+++ bin/cpuUsage2.sh
@@ -4,7 +4,9 @@ while true; do
         #mpstat -P 1,2,3,4,5,6,7,8,9,10,11,12,13,14  2>&1 | tee -a cpuUsage.txt
         #mkdir -p cpuInfo/
         #docker stats --format "{{.Container}}: {{.CPUPerc}}"
-        sudo docker stats --format "{{.Container}}: {{.CPUPerc}}" 2>&1 | tee -a cpuUsage.txt 
+        #sudo docker stats --format "{{.Name}}: {{.CPUPerc}}" 2>&1 | tee -a cpuUsage.txt 
+
+        sudo docker stats server1.1 --no-stream --format "{{.CPUPerc}}: {{ json . }}" 2>&1 | tee -a cpuUsage.txt 
         #sleep 1
        # echo q | htop -d 10| aha --black --line-fix > cpuInfo/htop${i}.html
        # ((i=i+1))
