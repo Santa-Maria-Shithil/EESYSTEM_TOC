b89c3363ff470dc9eda3d7439bce7333f5f246fc
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2022-12-20 19:14:58 -0500

    upate

--- bin/initClient.sh
+++ bin/initClient.sh
@@ -6,13 +6,13 @@ MASTERPORT=5087
 CLIENTNAME="client1"  #modify client name
 CLIENTIP="172.76.0.20" #ip start with IP address 172.75.0.200
 CLIENTPORT=5099 #port start with 5090
-NCLIENTS=90 # minimum 1
+NCLIENTS=100 # minimum 1
 NREQ=100000 # total number of req per client
 docker stop ${CLIENTNAME}
 docker rm ${CLIENTNAME}
 docker rmi ${CLIENTNAME}
 docker build --tag ${CLIENTNAME} .
-docker run --cpuset-cpus="10-63" --memory="8g" --net Paxos_Network1 --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e TYPE="client" --name ${CLIENTNAME} ${CLIENTNAME}
+docker run --cpuset-cpus="23-63" --memory="8g" --net Paxos_Network1 --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e TYPE="client" --name ${CLIENTNAME} ${CLIENTNAME}
 
 
 
--- bin/initServer.sh
+++ bin/initServer.sh
@@ -3,12 +3,14 @@
 MIP="172.76.0.2"
 MASTERNAME="master1"
 MASTERPORT=5087
-SERVERNAME="server1.3"  #modify serer name
-SERVERIP="172.76.0.14" #ip start with IP address 172.76.0.4
-SERVERPORT=5098 #port start with 5090
+SERVERNAME="server1.1"  #modify serer name
+SERVERIP="172.76.0.4" #ip start with IP address 172.76.0.4
+SERVERPORT=5090 #port start with 5090
+NET=Paxos_Network1 #network name start with Paxos_Network1
+
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
 
 #update cpus 35-42
-docker run --cpuset-cpus="14" --memory="8g" --net Paxos_Network1 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="3-6" --memory="8g" --net ${NET} --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
