21d15d829f7ca881d89fcd4ab12d5ae4482c7382
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2022-12-21 04:32:23 -0500

    update

--- bin/initServer.sh
+++ bin/initServer.sh
@@ -3,14 +3,14 @@
 MIP="172.80.0.2"
 MASTERNAME="master5"
 MASTERPORT=9087
-SERVERNAME="server5.2"  #modify serer name
-SERVERIP="172.80.0.5" #ip start with IP address 172.76.0.4
-SERVERPORT=9092 #port start with 5090
+SERVERNAME="server5.3"  #modify serer name
+SERVERIP="172.80.0.6" #ip start with IP address 172.76.0.4
+SERVERPORT=9093 #port start with 5090
 NET=Paxos_Network5 #network name start with Paxos_Network1
 
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
 
-#update cpus 19-22,3-7,7-10,15-18
-docker run --cpuset-cpus="3-7" --memory="8g" --net ${NET} --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+#update cpus 19-22,3-7,7-10,15-18,11-14
+docker run --cpuset-cpus="8-11" --memory="8g" --net ${NET} --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
