0eb64622b16f68d05c6521eca4ca85b95935f6bd
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2022-12-20 19:26:29 -0500

    update

--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #bash bin/initServer.sh
-MIP="172.76.0.2"
-MASTERNAME="master1"
-MASTERPORT=5087
-SERVERNAME="server1.5"  #modify serer name
-SERVERIP="172.76.0.8" #ip start with IP address 172.76.0.4
-SERVERPORT=5094 #port start with 5090
-NET=Paxos_Network1 #network name start with Paxos_Network1
+MIP="172.77.0.2"
+MASTERNAME="master2"
+MASTERPORT=6087
+SERVERNAME="server2.1"  #modify serer name
+SERVERIP="172.77.0.4" #ip start with IP address 172.76.0.4
+SERVERPORT=6090 #port start with 5090
+NET=Paxos_Network2 #network name start with Paxos_Network1
 
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
 
 #update cpus 35-42
-docker run --cpuset-cpus="19-22" --memory="8g" --net ${NET} --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="3-6" --memory="8g" --net ${NET} --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
