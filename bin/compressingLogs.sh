90a178d577aead31e2ceee1371558fe2c62a3982
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2023-02-05 19:45:46 -0500

    update

--- bin/compressingLogs.sh
+++ bin/compressingLogs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+sudo rm -r logs1.zip
 zip -r logs1.zip logs1
-zip -r logs2.zip logs2
-zip -r logs3.zip logs3
-zip -r logs4.zip logs4
-zip -r logs5.zip logs5
+#zip -r logs2.zip logs2
+#zip -r logs3.zip logs3
+#zip -r logs4.zip logs4
+#zip -r logs5.zip logs5
