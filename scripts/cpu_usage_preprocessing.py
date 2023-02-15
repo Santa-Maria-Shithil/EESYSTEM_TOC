520a3a044d2f10ea06faaeb4c41e7e351d40f334
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2023-02-14 21:12:14 -0500

    update

---
+++ scripts/cpu_usage_preprocessing.py
@@ -0,0 +1,102 @@
+ 
+i=1
+j=2
+maxi=0
+maxj=0
+x=[]
+y=[]
+
+y.append(0.0)
+
+x.append(0)
+
+
+a=[]
+
+a.append(0.0)
+
+b=[]
+
+b.append(0.0)
+
+c=[]
+
+c.append(0.0)
+
+
+d=[]
+
+d.append(0.0)
+
+sum1=[]
+sum2=[]
+sum3=[]
+sum4=[]
+sum5=[]
+sorted_array=[0,0,0,0,0]
+tput_median=[]
+for x in range(0,1000):
+    sum1.append(0.0)
+    sum2.append(0.0)
+    sum3.append(0.0)
+    sum4.append(0.0)
+    sum5.append(0.0)
+
+j=0
+for i in range(1,6):
+    myfile = open("E:\\tf_conflict\\run"+str(i)+"\cpuUsage.txt")
+    j=0
+    for line in myfile:
+        line.strip()
+        #print(line)
+        latency=line.split(":")
+
+
+        #latency[2].lstrip(" ")
+        #print(latency[0])
+
+        try: 
+            if latency[0]!="master5" and latency[0]!="client1":
+
+                l=latency[1].split("%")
+                #print(i)
+                if i==1:
+                    #print(l[0])
+                    sum1[j]=sum1[j]+float(l[0])
+                if i==2:
+                    sum2[j]=sum2[j]+float(l[0])
+                if i==3:
+                    sum3[j]=sum3[j]+float(l[0])
+                if i==4:
+                    sum4[j]=sum4[j]+float(l[0])
+                if i==5:
+                    sum5[j]=sum5[j]+float(l[0])
+            else:
+                j=j+1
+
+        except Exception as e:
+            print("Oops!", e.__class__, "occurred.")
+            print("Next entry.")
+            print()
+        if j==700:
+            break
+    # plotting the points 
+    myfile.close()
+  
+#print(sum5)
+myfile = open("cpu_usage_median.txt","w")
+
+for x in range(0,700):
+    sorted_array[0]=sum1[x]
+    sorted_array[1]=sum2[x]
+    sorted_array[2]=sum3[x]
+    sorted_array[3]=sum4[x]
+    sorted_array[4]=sum5[x]
+    
+    sorted_array.sort()
+    
+    print(sorted_array)
+    
+    myfile.write(str(sorted_array[3])+"\n")
+                     
+myfile.close()
