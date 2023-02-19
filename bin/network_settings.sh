7f12ea82b7bbb4fcfb23966bfa81246702b8e9fe
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-19 17:30:57 -0500

    update

---
+++ bin/network_settings.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo 'net.core.wmem_max=12582912' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.core.rmem_max=12582912' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.ipv4.tcp_rmem= 10240 87380 12582912' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.ipv4.tcp_wmem= 10240 87380 12582912' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.ipv4.udp_mem= 10240 87380 12582912' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.ipv4.tcp_window_scaling = 1' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.ipv4.tcp_sack = 1' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.ipv4.tcp_no_metrics_save = 1' >> /home/users/sshithil/sysctl.conf
+
+echo 'net.core.netdev_max_backlog = 100000' >> /home/users/sshithil/sysctl.conf
+
+sudo sysctl -p /home/users/sshithil/sysctl.conf
+
+
