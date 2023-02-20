5539e7845c67d17506e2a6801f4e0c40fb221d18
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-19 20:48:18 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -13,6 +13,6 @@ docker network rm ${NET}
 docker build --tag ${MASTERNAME} .
 docker network create --subnet=${SUBNET} ${NET}
 
-docker run --cap-add=NET_ADMIN --cpuset-cpus="1,2" --memory="8g" --net ${NET} --ip=${MIP} -p ${PORT}:${PORT} -e TYPE="master" -e MADDR=${MIP} -e MPORT=${PORT} -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME} 
+docker run --cap-add NET_ADMIN --cpuset-cpus="1,2" --memory="8g" --net ${NET} --ip=${MIP} -p ${PORT}:${PORT} -e TYPE="master" -e MADDR=${MIP} -e MPORT=${PORT} -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME} 
 #gaiadocker/iproute2 qdisc del dev eth0 root netem
 
