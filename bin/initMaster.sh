1fed7315d950fd43f2582bb665a61d6c59494fb0
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-20 19:21:18 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -8,13 +8,12 @@ SUBNET=10.0.1.0/24
 NET=overnet
 
 docker rm ${MASTERNAME}
-docker rmi ${MASTERNAME}
-#docker network rm ${NET}
+docker rmi ${MASTERNAME}docker network rm ${NET}
 docker build --tag ${MASTERNAME} .
 #docker network create --subnet=${SUBNET} ${NET}
-#docker network create -d overlay overnet
+docker network create  -d overlay --attachable overnet
 
 
-sudo docker run  --cpuset-cpus="1,2" --memory="8g" --net ${NET} --ip=${MIP} -p ${PORT}:${PORT} --cap-add=NET_ADMIN -e TYPE="master" -e MADDR=${MIP} -e MPORT=${PORT} -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME} 
+docker run  --cpuset-cpus="1,2" --memory="8g" --net ${NET} --ip=${MIP} -p ${PORT}:${PORT} --cap-add=NET_ADMIN -e TYPE="master" -e MADDR=${MIP} -e MPORT=${PORT} -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME} 
 #gaiadocker/iproute2 qdisc del dev eth0 root netem
 
