c0b805c8f13f4d534b8ba0b505f1a6c75384a021
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-23 15:23:56 -0500

    update

--- bin/initClient.sh
+++ bin/initClient.sh
@@ -7,7 +7,7 @@ CLIENTNAME="client1"  #modify client name
 CLIENTIP="10.0.1.20" #ip start with IP address 172.75.0.200
 CLIENTPORT=5099 #port start with 5090
 NCLIENTS=30 # minimum 1
-NREQ=1000000 # total number of req per client
+NREQ=100000 # total number of req per client
 docker stop ${CLIENTNAME}
 docker rm ${CLIENTNAME}
 docker rmi ${CLIENTNAME}
