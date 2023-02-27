ba4b401fe6487fb250ae7bd269f189fa8eb118ec
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-27 15:14:27 -0500

    update

--- bin/client.sh
+++ bin/client.sh
@@ -2,7 +2,7 @@
 
 
   for i in $(seq 1 10); do
-        bin/eclientol -maddr 130.245.173.103 -mport 7087 -runtime 100 -id ${i} 2>&1 | tee -a logs/c_${i}.txt ${ALL} >/dev/null &
+        bin/eclientol -maddr 130.245.173.103 -mport 7087 -q 50000 -c 0 -runtime 100 -target_rps 1000 -id ${i} 2>&1 | tee -a logs/c_${i}.txt ${ALL} >/dev/null &
         #${DIR}/clientmain ${args} -id  2>&1 | tee -a logs/c_${i}.txt ${ALL} >/dev/null &
         echo "> Client $i of ${NCLIENTS} started!"
     done
