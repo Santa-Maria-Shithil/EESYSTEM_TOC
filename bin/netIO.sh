c55e6ef1b87e5cacc9f68f2bda1dd33d30c028e8
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 14:56:48 -0500

    update

--- bin/netIO.sh
+++ bin/netIO.sh
@@ -3,7 +3,7 @@ sudo rm -r overall_netIO.txt
 i=0
 while true; do
         
-        sudo docker stats --no-stream --format "{{.Name}}:{{.NetIO}}" 2>&1 | tee -a netIO.txt 
+        sudo docker stats  --format "{{.Name}}:{{.NetIO}}" 2>&1 | tee -a netIO.txt 
 
        # sudo vnstat -l --style 0 -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
      
