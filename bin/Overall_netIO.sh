73d5c4fc9240eed51df3c53b1a1a5a7cb291ca26
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 16:58:13 -0500

    update

--- bin/Overall_netIO.sh
+++ bin/Overall_netIO.sh
@@ -1,3 +1,3 @@
 sudo rm -r overall_netIO.txt        
         
-sudo vnstat -l --style 0 -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
+sudo vnstat -l -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
