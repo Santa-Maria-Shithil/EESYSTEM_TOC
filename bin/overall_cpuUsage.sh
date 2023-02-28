51222c8f1e289393cd873254c112465b68fed15a
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 16:50:03 -0500

    update

--- bin/overall_cpuUsage.sh
+++ bin/overall_cpuUsage.sh
@@ -5,5 +5,5 @@ i=0
 while true; do
 
         sudo top -bn1 | grep "Cpu(s)" |sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |awk '{print 100 - $1}' 2>&1 | tee -a Overall_cpuUsage.txt 
-        sleep1
+        sleep 1
     done
