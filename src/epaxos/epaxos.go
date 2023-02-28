1b6601fd333eca2354288e65352ecdf412f40b13
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-27 23:30:55 -0500

    update

--- src/epaxos/epaxos.go
+++ src/epaxos/epaxos.go
@@ -1267,7 +1267,7 @@ func (r *Replica) handlePreAcceptOK(pareply *epaxosproto.PreAcceptOK) {
 					&genericsmrproto.ProposeReplyTS{
 						TRUE,
 						inst.lb.clientProposals[i].CommandId,
-						0,
+						state.NIL,
 						inst.lb.clientProposals[i].Timestamp},
 					inst.lb.clientProposals[i].Reply)
 			}
@@ -1382,7 +1382,7 @@ func (r *Replica) handleAcceptReply(areply *epaxosproto.AcceptReply) {
 					&genericsmrproto.ProposeReplyTS{
 						TRUE,
 						inst.lb.clientProposals[i].CommandId,
-						1,
+						state.CFT,
 						inst.lb.clientProposals[i].Timestamp},
 					inst.lb.clientProposals[i].Reply)
 			}
