7c64b521471134a34a665ada88f395cd2b9d4ecf
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 17:33:26 -0500

    update

--- bin/Overall_netIO.sh
+++ bin/Overall_netIO.sh
@@ -1,5 +1,7 @@
 sudo rm -r overall_netIO.txt     
 
 while true; do        
-sudo vnstat -l -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
+##sudo vnstat -l -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
+sudo vnstat -tr -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
+
 done
