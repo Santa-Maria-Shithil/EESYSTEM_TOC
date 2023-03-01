403ac47b6d07f13f79ac38abbbd491b5db02dac2
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 23:30:47 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,4 @@
 sudo ip link set eno1 qlen 300000
 sudo sysctl  net.core.somaxconn=33554432  #32MB
-sudo sysctl net.core.netdev_max_backlog=3554432
-sudo sysctl net.ipv4.tcp_max_syn_backlog=3554432
+sudo sysctl net.core.netdev_max_backlog=33554432 
+sudo sysctl net.ipv4.tcp_max_syn_backlog=33554432 
--- bin/initClient.sh
+++ bin/initClient.sh
@@ -6,7 +6,7 @@ MASTERPORT=9087
 CLIENTNAME="client1"  #modify client name
 CLIENTIP="10.0.1.20" #ip start with IP address 172.75.0.200
 CLIENTPORT=5099 #port start with 5090
-NCLIENTS=40 # minimum 1
+NCLIENTS=30 # minimum 1
 NREQ=200000 # total number of req per client
 NRUNTIME=300
 docker stop ${CLIENTNAME}
@@ -15,7 +15,7 @@ docker rmi ${CLIENTNAME}
 docker build --tag ${CLIENTNAME} .
 #docker run --cpuset-cpus="13-63" --memory="8g" --net Paxos_Network5 --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e TYPE="client" --name ${CLIENTNAME} ${CLIENTNAME}
 #docker run --privileged --cap-add=NET_ADMIN --net overnet --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e TYPE="client"  --name ${CLIENTNAME} ${CLIENTNAME}
-docker run --privileged --cap-add=NET_ADMIN --net overnet --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} --sysctl=net.core.somaxconn=12582912 --sysctl=net.ipv4.tcp_max_syn_backlog=12582912  -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e NRUNTIME=${NRUNTIME} -e TYPE="client"  --name ${CLIENTNAME} ${CLIENTNAME}
+docker run --privileged --cap-add=NET_ADMIN --net overnet --ip=${CLIENTIP} -p ${CLIENTPORT}:${CLIENTPORT} --sysctl=net.core.somaxconn=33554432 --sysctl=net.ipv4.tcp_max_syn_backlog=33554432  -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e Q=${NREQ} -e NCLIENTS=${NCLIENTS} -e NRUNTIME=${NRUNTIME} -e TYPE="client"  --name ${CLIENTNAME} ${CLIENTNAME}
 
 
 
