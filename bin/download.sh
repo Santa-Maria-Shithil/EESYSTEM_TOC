c5d42e45afc190da2e6e157fdc38537bebddca39
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 20:28:08 -0500

    update

---
+++ bin/download.sh
@@ -0,0 +1,5 @@
+scp sshithil@130.245.173.104:/home/users/sshithil/copilot/logs1.zip ./Downloads 
+scp sshithil@130.245.173.104:/home/users/sshithil/copilot/cpuUsage.txt ./Downloads 
+scp sshithil@130.245.173.104:/home/users/sshithil/copilot/Overall_cpuUsage.txt ./Downloads 
+scp sshithil@130.245.173.104:/home/users/sshithil/copilot/Overall_netIO.txt ./Downloads 
+scp sshithil@130.245.173.104:/home/users/sshithil/copilot/netIO.txt ./Downloads
