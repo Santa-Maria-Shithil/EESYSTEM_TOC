d444ca4dfb15db1e5617e11a0651fb6b23594c6a
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-02-28 22:26:07 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,4 @@
-sudo ip link set eno1 qlen 50000
+sudo ip link set eno1 qlen 100000
 sudo sysctl  net.core.somaxconn=12582912
 sudo sysctl net.core.netdev_max_backlog=12582912
 sudo sysctl net.ipv4.tcp_max_syn_backlog=12582912
