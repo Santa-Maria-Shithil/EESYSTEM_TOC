6b3982bd02a78cad7bec65819a1eac71c134f74e
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-01 12:07:37 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -3,16 +3,16 @@ sudo sysctl  net.core.somaxconn=100000    #16777216  #32MB
 sudo sysctl net.core.netdev_max_backlog=100000    #16777216
 sudo sysctl net.ipv4.tcp_max_syn_backlog=100000    ###16777216
 
-sudo sysctl -w net.core.wmem_max=134217728
+sudo sysctl  net.core.wmem_max=134217728
 
-sudo sysctl -w net.core.rmem_max=134217728
+sudo sysctl net.core.rmem_max=134217728
 
-sudo sysctl -w net.ipv4.tcp_rmem="10240 12582912 134217728"
+sudo sysctl  net.ipv4.tcp_rmem="10240 12582912 134217728"
 
-sudo sysctl -w net.ipv4.tcp_wmem="10240 12582912 134217728"
+sudo sysctl  net.ipv4.tcp_wmem="10240 12582912 134217728"
 
-sudo sysctl -w net.ipv4.tcp_window_scaling=1
+sudo sysctl net.ipv4.tcp_window_scaling=1
 
-sudo sysctl -w net.ipv4.tcp_sack=1
+sudo sysctl  net.ipv4.tcp_sack=1
 
-sudo sysctl -w net.ipv4.tcp_no_metrics_save=1
+sudo sysctl  net.ipv4.tcp_no_metrics_save=1
