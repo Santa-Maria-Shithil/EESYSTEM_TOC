7b06959548805fe1afc1a234b26be67c0b830a68
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-01 00:50:21 -0500

    updateupdate

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,20 @@
 sudo ip link set eno1 qlen 500000
-sudo sysctl  net.core.somaxconn=134217728  #32MB
-sudo sysctl net.core.netdev_max_backlog=134217728
-sudo sysctl net.ipv4.tcp_max_syn_backlog=134217728
+sudo sysctl -w  net.core.somaxconn=134217728  #32MB
+sudo sysctl -w net.core.netdev_max_backlog=134217728
+sudo sysctl -w net.ipv4.tcp_max_syn_backlog=134217728
+
+sudo sysctl -w net.core.wmem_max=134217728
+
+sudo sysctl -w net.core.rmem_max=134217728
+
+sudo sysctl -w net.ipv4.tcp_rmem="10240 12582912 134217728"
+
+sudo sysctl -w net.ipv4.tcp_wmem="10240 12582912 134217728"
+
+sudo sysctl -w net.ipv4.udp_mem="10240 12582912 134217728"
+
+sudo sysctl -w net.ipv4.tcp_window_scaling=1
+
+sudo sysctl -w net.ipv4.tcp_sack=1
+
+sudo sysctl -w net.ipv4.tcp_no_metrics_save=1
