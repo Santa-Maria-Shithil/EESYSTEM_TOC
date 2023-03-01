8e9a87d17a8344c4a0b10c84b2fc0a0494c4a7e1
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-01 12:03:54 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -1,18 +1,18 @@
 sudo ip link set eno1 qlen 100000
-sudo sysctl  net.core.somaxconn=16777216  #32MB
-sudo sysctl net.core.netdev_max_backlog=16777216
-sudo sysctl net.ipv4.tcp_max_syn_backlog=16777216
+sudo sysctl  net.core.somaxconn=100000    #16777216  #32MB
+sudo sysctl net.core.netdev_max_backlog=100000    #16777216
+sudo sysctl net.ipv4.tcp_max_syn_backlog=100000    ###16777216
 
-#sudo sysctl -w net.core.wmem_max=134217728
+sudo sysctl -w net.core.wmem_max=134217728
 
-#sudo sysctl -w net.core.rmem_max=134217728
+sudo sysctl -w net.core.rmem_max=134217728
 
-#sudo sysctl -w net.ipv4.tcp_rmem="10240 12582912 134217728"
+sudo sysctl -w net.ipv4.tcp_rmem="10240 12582912 134217728"
 
-#sudo sysctl -w net.ipv4.tcp_wmem="10240 12582912 134217728"
+sudo sysctl -w net.ipv4.tcp_wmem="10240 12582912 134217728"
 
-#sudo sysctl -w net.ipv4.tcp_window_scaling=1
+sudo sysctl -w net.ipv4.tcp_window_scaling=1
 
-#sudo sysctl -w net.ipv4.tcp_sack=1
+sudo sysctl -w net.ipv4.tcp_sack=1
 
-#sudo sysctl -w net.ipv4.tcp_no_metrics_save=1
+sudo sysctl -w net.ipv4.tcp_no_metrics_save=1
