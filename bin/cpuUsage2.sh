3176b6a10fc03d082dc067743fdaa675cb71a3de
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-03 12:51:26 -0500

    update

--- bin/cpuUsage2.sh
+++ bin/cpuUsage2.sh
@@ -14,7 +14,7 @@ while true; do
 
         #sudo top -bn1 | grep "Cpu(s)" |sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |awk '{print 100 - $1}' 2>&1 | tee -a Overall_cpuUsage.txt 
         
-        #sleep 1
+        sleep 1
        # echo q | htop -d 10| aha --black --line-fix > cpuInfo/htop${i}.html
        # ((i=i+1))
 
