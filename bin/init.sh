e534f232502325ffa1da7329f37aec181c210a2c
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-02 23:26:35 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,11 @@
-sudo ip link set eno1 qlen 200000
-sudo sysctl  net.core.somaxconn=200000 #16777216  #32MB
-sudo sysctl net.core.netdev_max_backlog=200000 #16777216
-sudo sysctl net.ipv4.tcp_max_syn_backlog=200000 #16777216
+sudo ip link set eno1 qlen 300000
+sudo sysctl  net.core.somaxconn=300000 #16777216  #32MB
+sudo sysctl net.core.netdev_max_backlog=300000 #16777216
+sudo sysctl net.ipv4.tcp_max_syn_backlog=300000 #16777216
+sudo ethtool -C eno1 tx-usecs 122
+sudo ethtool -C eno1 rx-usecs 62
+sudo ethtool -G eno1 tx 511 
+sudo ethtool -G eno1 rx 2047 
 
 #sudo sysctl  net.core.wmem_max=134217728
 
