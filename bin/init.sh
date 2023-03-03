9ca1718bed5c3ca1fe173b5363157e1c99ced399
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-02 23:50:14 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,7 @@
-sudo ip link set eno1 qlen 500000
-sudo sysctl  net.core.somaxconn=500000 #16777216  #32MB
-sudo sysctl net.core.netdev_max_backlog=500000 #16777216
-sudo sysctl net.ipv4.tcp_max_syn_backlog=500000 #16777216
+sudo ip link set eno1 qlen 300000
+sudo sysctl  net.core.somaxconn=300000 #16777216  #32MB
+sudo sysctl net.core.netdev_max_backlog=300000 #16777216
+sudo sysctl net.ipv4.tcp_max_syn_backlog=300000 #16777216
 sudo ethtool -C eno1 tx-usecs 122
 sudo ethtool -C eno1 rx-usecs 62
 sudo ethtool -G eno1 tx 511 
