5ee18050587269a23759603de5c869f40d9e3494
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-03 00:08:02 -0500

    update

--- bin/init.sh
+++ bin/init.sh
@@ -2,6 +2,7 @@ sudo ip link set eno1 qlen 300000
 sudo sysctl  net.core.somaxconn=300000 #16777216  #32MB
 sudo sysctl net.core.netdev_max_backlog=300000 #16777216
 sudo sysctl net.ipv4.tcp_max_syn_backlog=300000 #16777216
+sudo sysctl net.core.netdev_budget=500
 sudo ethtool -C eno1 tx-usecs 122
 sudo ethtool -C eno1 rx-usecs 62
 sudo ethtool -G eno1 tx 511 
@@ -11,9 +12,9 @@ sudo ethtool -G eno1 rx 2047
 
 #sudo sysctl net.core.rmem_max=134217728
 
-#sudo sysctl  net.ipv4.tcp_rmem="10240 12582912 134217728"
+sudo sysctl  net.ipv4.tcp_rmem="4096 262144 16777216"
 
-#sudo sysctl  net.ipv4.tcp_wmem="10240 12582912 134217728"
+sudo sysctl  net.ipv4.tcp_wmem="4096 262144 16777216"
 
 #sudo sysctl net.ipv4.tcp_window_scaling=1
 
