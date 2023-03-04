fa529bebd4151d7c5f2f499da59938267a763bb7
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-04 18:32:54 -0500

    update

--- bin/overall_cpuUsage_server.sh
+++ bin/overall_cpuUsage_server.sh
@@ -4,6 +4,6 @@
 i=0
 while true; do
 
-        sudo top -bn1 | grep "Cpu(s)" |sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |awk '{print 100 - $1}' 2>&1 | tee -a Overall_cpuUsage_server.txt 
+        top -bn1 | grep "Cpu(s)" |sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |awk '{print 100 - $1}' 2>&1 | tee -a Overall_cpuUsage_server.txt 
         sleep 1
     done
--- bin/overall_netIO_client.sh
+++ bin/overall_netIO_client.sh
@@ -2,6 +2,6 @@
 
 while true; do        
 ##sudo vnstat -l -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
-sudo vnstat --style 1  -tr 2 -i eno1 2>&1 | tee -a Overall_netIO_client.txt #need to run it separately
+vnstat --style 1  -tr 2 -i eno1 2>&1 | tee -a Overall_netIO_client.txt #need to run it separately
 
 done
