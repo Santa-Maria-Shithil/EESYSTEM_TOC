e49e4656718a30d6dc77e104a3b19fd03f68524b
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-04 17:45:08 -0500

    update

--- bin/netIO_server.sh
+++ bin/netIO_server.sh
@@ -3,7 +3,7 @@
 i=0
 while true; do
         
-        sudo docker stats --no-stream --format "{{.Name}}:{{.NetIO}}" 2>&1 | tee -a netIO_server.txt 
+        docker stats --no-stream --format "{{.Name}}:{{.NetIO}}" 2>&1 | tee -a netIO_server.txt 
         #sudo nethogs  -t -d 1 -c 2| grep "sshithil"
        # sudo vnstat -l --style 0 -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
      
