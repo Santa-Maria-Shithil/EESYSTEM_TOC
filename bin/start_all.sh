8ddde4f824d7719185f1f225c86bcb7727abaa01
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-04 15:17:53 -0500

    update

--- bin/start_all.sh
+++ bin/start_all.sh
@@ -4,4 +4,4 @@ bash bin/netIO.sh 3 &
 bash bin/network_matrics_tracking.sh 4 &
 bash bin/overall_cpuUsage.sh 5 &
 bash bin/overall_mem_usage.sh 6 &
-bash bin/overall_netIO.sh 7 &
+bash bin/Overall_netIO.sh 7 &
