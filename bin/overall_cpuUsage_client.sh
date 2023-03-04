a592e5f1d217982d76a989689182a33faa714d8c
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-04 17:36:47 -0500

    update

--- bin/overall_cpuUsage_client.sh
+++ bin/overall_cpuUsage_client.sh
@@ -4,6 +4,6 @@
 i=0
 while true; do
 
-        sudo top -bn1 | grep "Cpu(s)" |sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |awk '{print 100 - $1}' 2>&1 | tee -a Overall_cpuUsage_clients.txt 
+        sudo top -bn1 | grep "Cpu(s)" |sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |awk '{print 100 - $1}' 2>&1 | tee -a Overall_cpuUsage_client.txt 
         sleep 1
     done
