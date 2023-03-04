71f38dcf9f23836029932310ced60b0971c15494
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-03-04 13:46:11 -0500

    update

---
+++ bin/network_matrics_tracking.sh
@@ -0,0 +1,23 @@
+sudo rm -r nic_info.txt
+sudo rm -r cpu_intr_info.txt
+sudo rm -r softIQR_info.txt
+sudo rm -r netstat_statistics.txt
+sudo rm -r netstat_eno1_statistics.txt
+sudo rm -r SR_queue.txt
+
+while true; do        
+##sudo vnstat -l -i eno1 2>&1 | tee -a Overall_netIO.txt #need to run it separately
+sudo ethtool --statistics eno1 | grep "tx_discards" 2>&1 | tee -a nic_info.txt #need to run it separately
+sudo ethtool --statistics eno1 | grep "rx_discards" 2>&1 | tee -a nic_info.txt
+
+egrep "CPU|eth0" /proc/interrupts  2>&1 | tee -a cpu_intr_info.txt
+
+cat /proc/net/softnet_stat  2>&1 | tee -a softIQR_info.txt
+
+netstat -s  2>&1 | tee -a netstat_statistics.txt
+
+netstat -i eno1 2>&1 | tee -a netstat_eno1_statistics.txt
+
+ss  -npt 2>&1 | tee -a SR_queue.txt
+
+done
