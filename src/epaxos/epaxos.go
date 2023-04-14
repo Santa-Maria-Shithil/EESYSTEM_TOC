37babd12643a37aee1217c5086d8e3e659df6e32
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-04-14 01:40:49 -0400

    update

--- src/epaxos/epaxos.go
+++ src/epaxos/epaxos.go
@@ -506,7 +506,7 @@ func (r *Replica) run() {
 ************************************/
 
 func (r *Replica) executeCommands() {
-	const SLEEP_TIME_NS = 1000 // 1 microsecond=1000, now 1us
+	const SLEEP_TIME_NS = 1000 * 1000 // 1 microsecond=1000, now 1ms
 	problemInstance := make([]int32, r.N)
 	timeout := make([]uint64, r.N)
 	for q := 0; q < r.N; q++ {
