7ddf49b6258e3e5a0644881281ad7b1d0548c18d
Santa-Maria-Shithil <shithil@cs.stonybrook.edu>
2023-08-10 21:15:50 -0400

    update

--- src/epaxos/epaxos.go
+++ src/epaxos/epaxos.go
@@ -1033,8 +1033,8 @@ func (r *Replica) startPhase1(replica int32, instance int32, ballot int32, propo
 		log.Printf("previos value seq=%d, instance=%d", seq1, instance1)
 		log.Printf("current value seq=%d, instance=%d", seq, instance)
 		r.bcastPreAccept(r.Id, instance, ballot, cmds, seq, deps)
-		time.Sleep((10 * time.Second))
-		//r.bcastPreAccept(id1, instance1, ballot1, cmds1, seq1, deps1)
+		//time.Sleep((10 * time.Second))
+		r.bcastPreAccept(id1, instance1, ballot1, cmds1, seq1, deps1)
 
 	}
 
